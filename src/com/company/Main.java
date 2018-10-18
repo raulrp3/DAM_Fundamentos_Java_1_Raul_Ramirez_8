2afda8cfd446193261745e1025772dee1325f0c5
raulrp3 <raul.ramirez@escuelaestech.es>
2018-10-18 16:26:44 +0200

    Cerrando "scanner"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -31,6 +31,7 @@ public class Main {
         primerNumero = validarNumero(sc);
         System.out.print("Introduce un segundo número: ");
         segundoNumero = validarNumero(sc);
+        sc.close();
         System.out.println(ordenarNumeros(primerNumero, segundoNumero));
     }
 }
